8dbf6d89137df677f88a5222b790d044844771cc
e1len-code <160895@unsaac.edu.pe>
2024-04-01 16:44:47 -0500

    fix: change with Response in LogController

--- ControlBS.WebApi/Controllers/LogController.cs
+++ ControlBS.WebApi/Controllers/LogController.cs
@@ -4,6 +4,7 @@ using ControlBS.Facade;
 using Serilog;
 using FluentValidation;
 using ControlBS.WebApi.Utils.Auth;
+using ControlBS.BusinessObjects;
 
 namespace ControlBS.WebApi.Controllers
 {
@@ -13,15 +14,17 @@ namespace ControlBS.WebApi.Controllers
     public class LogController : ControllerBase
     {
         private IHostEnvironment Environment;
+        private Response<ErrorResponse> errorResponse;
         private ErrorResponse? error;
 
 
         public LogController(IWebHostEnvironment _environment)
         {
+            errorResponse = new Response<ErrorResponse>();
             Environment = _environment;
         }
 
-        [HttpGet]
+        [HttpGet("{datetime}")]
         public IActionResult GetLog(string datetime)
         {
             //Format datetime String YYYYMMDD
@@ -31,18 +34,20 @@ namespace ControlBS.WebApi.Controllers
                 // Check if file exists
                 if (!System.IO.File.Exists(_logFilePath))
                 {
-                    error = new ErrorResponse { message = String.Format("No se ha encontrado el archivo de la fecha {0}  .log", datetime), source = "GetLog - LogController", stackTrace = "" };
+                    error = new ErrorResponse { message = String.Format("No se ha encontrado el archivo de fecha {0}.log", datetime), source = "GetLog - LogController", stackTrace = "" };
+                    errorResponse.statusCode = System.Net.HttpStatusCode.NotFound;
+                    errorResponse.errors.Add(error);
                     Log.Error(error.ToString());
-                    return StatusCode(StatusCodes.Status404NotFound, error.ToString());
+                    return StatusCode(StatusCodes.Status404NotFound, errorResponse);
                 }
                 var fileStream = new FileStream(_logFilePath, FileMode.Open, FileAccess.Read);
                 return File(fileStream, "text/plain", Path.GetFileName(_logFilePath));
             }
             catch (Exception e)
             {
-                error = new ErrorResponse { message = e.Message, source = e.Source, stackTrace = e.StackTrace };
-                Log.Error(error.ToString());
-                return StatusCode(StatusCodes.Status500InternalServerError, new { error.message });
+                errorResponse = new Response<ErrorResponse>(e);
+                Log.Error(errorResponse.errors.First().ToString());
+                return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
                 throw;
             }
         }
